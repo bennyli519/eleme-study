2210b848fab1b5cdec331adfb2cac66d28aa98c4
jerry <519248932@qq.com>
2017-11-16 16:20:15 +0800

    better-scroll运用

--- src/components/good/goods.vue
+++ src/components/good/goods.vue
@@ -6,18 +6,18 @@ Description
 -->
 <template>
     <div class="goods">
-       <div class="menu-wrapper">
+       <div class="menu-wrapper" ref="menu">
          <ul>
-           <li v-for="item in goods" class="menu-item">
+           <li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex===index}" @click="selectMenu(index,$event)">
              <span class="text border-1px">
                <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{ item.name }}
              </span>
            </li>
          </ul>
        </div>
-       <div class="foods-wrapper">
+       <div class="foods-wrapper"  ref="food">
          <ul>
-           <li v-for="item in goods" class="food-list">
+           <li v-for="item in goods" class="food-list food-list-hook">
               <h1 class="title">{{item.name}}</h1>
               <ul>
                 <li v-for="food in item.foods" class="food-item  border-1px">
@@ -45,6 +45,7 @@ Description
 </template>
 
 <script>
+  import BScroll from 'better-scroll'
   const ERR_OK = 0
   export default {
     prop:{
@@ -54,7 +55,21 @@ Description
     },
     data(){
       return {
-        goods:[]
+        goods:[],
+        listHeight:[],
+        scrollY:0
+      }
+    },
+    computed:{
+      currentIndex(){
+        for(let i=0;i < this.listHeight.length; i++){
+          let height1 = this.listHeight[i]
+          let height2 = this.listHeight[i+1]
+          if(!height2 || (this.scrollY >= height1 && this.scrollY < height2)){
+            return i;
+          }
+        }
+        return 0
       }
     },
     created(){
@@ -64,9 +79,46 @@ Description
           res = res.body
           if(res.errno == ERR_OK){
             this.goods = res.data
+            this.$nextTick(()=>{
+               this._initScroll()
+               this._calculateHeight()
+            })
           }
         }
       )
+    },
+    methods:{
+      _initScroll(){
+        this.menuScroll = new BScroll(this.$refs.menu,{
+          click:true
+        });
+        this.foodsScroll = new BScroll(this.$refs.food,{
+          probeType:3
+        });
+
+        this.foodsScroll.on('scroll',(pos)=>{
+          this.scrollY = Math.abs(Math.round(pos.y))
+        })
+      },
+      _calculateHeight(){
+        let foodList = this.$refs.food.getElementsByClassName('food-list-hook')
+        let height = 0
+        this.listHeight.push(height)
+        for(let i=0; i<foodList.length;i++){
+          let item = foodList[i]
+          height += item.clientHeight
+          this.listHeight.push(height)
+        }
+      },
+      selectMenu(index,event){
+        if(!event._constructed){
+          return 
+        }
+        let footList = this.$refs.food.getElementsByClassName('food-list-hook')
+        let el = footList[index]
+        this.foodsScroll.scrollToElement(el,300)
+        console.log(index)
+      }
     }
   }
 </script>
@@ -89,6 +141,14 @@ Description
           width :56px
           padding:0 12px
           line-height:14px
+          &.current
+            position:relative
+            z-index:10
+            margin-top:-1px
+            background:#fff
+            font-weight:700
+            .text
+              border-none()
           .icon
               display:inline-block
               width:12px
@@ -147,9 +207,10 @@ Description
               font-size:10px
               color:rgb(147,153,159)
             .desc
+              line-height:12px
               margin-bottom:8px
             .extra
-              &.count
+              .count
                 margin-right:12px
             .price
               font-weight:700
---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,17 @@
+<!--
+Description
+@authors Your Name (you@example.org)
+@date    2017-11-16 11:36:49
+@version 1.0.0
+-->
+<template>
+ 
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="css">
+  
+</style>
